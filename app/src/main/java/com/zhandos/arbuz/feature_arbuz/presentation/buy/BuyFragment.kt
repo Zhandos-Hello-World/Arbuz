5be84d03728a9d94486450034515ea9f18d18d49
Baimurat Zhandos <76952603+Zhandos-Hello-World@users.noreply.github.com>
2022-05-28 23:14:36 +0600

    Created Buy fragment

---
+++ app/src/main/java/com/zhandos/arbuz/feature_arbuz/presentation/buy/BuyFragment.kt
@@ -0,0 +1,18 @@
+package com.zhandos.arbuz.feature_arbuz.presentation.buy
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class BuyFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
